01d0862e7274c0209da1bd456cb80fe44c320c4e
pythonist-lab <soumyodip25@gmail.com>
2025-12-31 19:07:31 +0530

    Create room_rules.py

---
+++ room_rules.py
@@ -0,0 +1,80 @@
+minimum_room_sizes_india = {
+   "habitable_rooms": {
+       "living_room": {
+           "min_area_sqm": 9.5,
+           "min_width_m": 2.4,
+           "min_length_m": 3.0
+       },
+       "bedroom_single": {
+           "min_area_sqm": 9.5,
+           "min_width_m": 2.4,
+           "min_length_m": 3.0
+       },
+       "bedroom_double": {
+           "min_area_sqm": 11.5,
+           "min_width_m": 2.75,
+           "min_length_m": 3.5
+       },
+       "study_room": {
+           "min_area_sqm": 7.5,
+           "min_width_m": 2.1,
+           "min_length_m": 3.0
+       }
+   },
+   "kitchen_dining": {
+       "kitchen_separate": {
+           "min_area_sqm": 5.0,
+           "min_width_m": 1.8
+       },
+       "kitchenette": {
+           "min_area_sqm": 3.3,
+           "min_width_m": 1.5
+       },
+       "dining_area": {
+           "min_area_sqm": 7.5,
+           "min_width_m": 2.4
+       }
+   },
+   "toilets_bathrooms": {
+       "bathroom": {
+           "min_area_sqm": 1.8,
+           "min_width_m": 1.2
+       },
+       "wc": {
+           "min_area_sqm": 1.1,
+           "min_width_m": 0.9
+       },
+       "bath_wc_combined": {
+           "min_area_sqm": 2.8,
+           "min_width_m": 1.2
+       }
+   },
+   "circulation": {
+       "passage_corridor": {
+           "min_width_m": 0.9
+       },
+       "staircase_residential": {
+           "min_width_m": 0.9
+       },
+       "staircase_public": {
+           "min_width_m": 1.5
+       }
+   },
+   "other_spaces": {
+       "store_room": {
+           "min_width_m": 1.5
+       },
+       "balcony": {
+           "min_width_m": 1.0
+       },
+       "utility_area": {
+           "min_width_m": 1.5
+       }
+   },
+   "minimum_heights": {
+       "habitable_room_m": 2.75,
+       "kitchen_m": 2.6,
+       "bath_wc_m": 2.2,
+       "corridor_m": 2.1
+   }
+}
